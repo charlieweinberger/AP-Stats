dac86701bc322ef6d65279c09360e480a65be1f3
Charlie Weinberger <364charliebrown@gmail.com>
2022-08-31 22:42:02 +0000

    8/31 stuff

--- test.py
+++ test.py
@@ -18,18 +18,6 @@ print("\nAddison Rae's statistics:")
 rae_stats.all()
 print('')
 
-# The distribution of the number of views (in millions) that Charli D'Amelio has recieved on her 31 most recent tiktok videos is roughly symmetric.
-# The distribution of the number of views (in millions) that Addison Rae has recieved on her 31 most recent tiktok videos is skewed right.
-
-# A typical Charli D'Amelio tiktok has 20.1 million views.
-# A typical Addison Rae tiktok has 17.5 million views.
-
-# The middle 50% of Charli D'Amelio tiktoks vary in view count by 17.4 million views.
-# The middle 50% of Addison Rae tiktoks vary in view count by only 10.5 million views.
-
-# Charli D'Amelio has one outlier video, at 58.8 million views.
-# Addison Rae has two outlier videos, at 57.7 and 65.3 million views.
-
 """
 
 plt.histogram(data, max(data) - min(data))
@@ -62,5 +50,40 @@ male_class.all()
 female_class.all()
 
 The distribution of the heights of the female students in this AP Stats class is skewed left, whereas distribution of the heights of the male students is roughtly symmetric. A typical female student has a height of 66 inches, whereas a typical male student has a height of 70 inches. Male students tend to be taller than female students. Both female and male heights, among the middle 50%, vary by 3.5 inches. However, female heights have a range of 10 inches, which is slightly larger than the range of male heights, which is 9 inches. There are no male or female students with unusual heights.
+
+"""
+
+"""
+
+Example 2:
+
+The distribution of the number of views (in millions) that Charli D'Amelio has recieved on her 31 most recent tiktok videos is roughly symmetric.
+The distribution of the number of views (in millions) that Addison Rae has recieved on her 31 most recent tiktok videos is skewed right.
+
+A typical Charli D'Amelio tiktok has 20.1 million views.
+A typical Addison Rae tiktok has 17.5 million views.
+
+The middle 50% of Charli D'Amelio tiktoks vary in view count by 17.4 million views.
+The middle 50% of Addison Rae tiktoks vary in view count by only 10.5 million views.
+
+Charli D'Amelio has one outlier video, at 58.8 million views.
+Addison Rae has two outlier videos, at 57.7 and 65.3 million views.
+
+"""
+
+"""
+
+Example 3 (page 97 #16):
+
+From 2002 to 2004, the average weekly gas price at a service station in the midwestern United States increased. The range of the distributions also increased with time.
+
+The distribution in 2002 is symmetric. The typical gas price was about 1.35 dollars per gallon. There were 4 weeks during which gas prices were unusually low, three of which were extremely low.
+
+The distribution in 2003 is skewed right. The typical gas price was about 1.5 dollars per gallon.
+
+The distribution in 2004 is symmetric. The typical gas price was about 1.70 dollars per gallon. There was 1 week during which gas prices were unusually high.
+
+[add IQRs]
+[add values for outliers]
 
 """
